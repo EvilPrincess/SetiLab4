2666fe3b93db708b3a8f895cf4672904d26d669d
Никита Фомин <defdXs@yandex.ru>
2023-04-09 14:50:10 +0300

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,75 @@
+#include "main.h"
+
+using namespace std;
+
+//
+//		GLOBAL VARIABLES
+//
+HWND MainWnd = { };
+
+
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow) {
+
+	WNDCLASS MainWndClass = NewWindowClass((HBRUSH)COLOR_WINDOW, LoadCursor(NULL, IDC_ARROW), hInst,
+		LoadIcon(NULL, IDI_QUESTION), L"MainWndClass", MainWndProc);
+
+	if (!RegisterClassW(&MainWndClass)) { return -1; }
+
+	MSG MainWndMessage = { };
+
+	MainWnd = CreateWindow(L"MainWndClass", L"My Dumb Program 2.0", WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_VISIBLE, 100, 100, 1600, 900, NULL, NULL, NULL, NULL);
+	while (GetMessage(&MainWndMessage, NULL, NULL, NULL)) {
+		TranslateMessage(&MainWndMessage);
+		DispatchMessage(&MainWndMessage);
+	}
+}
+
+
+WNDCLASS NewWindowClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON Icon, LPCWSTR Name, WNDPROC Procedure) {
+	WNDCLASS NWC = { 0 };
+
+	NWC.hIcon = Icon;
+	NWC.hCursor = Cursor;
+	NWC.hInstance = hInst;
+	NWC.lpszClassName = Name;
+	NWC.hbrBackground = BGColor;
+	NWC.lpfnWndProc = Procedure;
+
+	return NWC;
+}
+
+
+LRESULT CALLBACK MainWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+		case WM_CREATE:
+		{
+			break;
+		}
+		case WM_CLOSE:
+		{
+			PostQuitMessage(0);
+			break;
+		}
+		case WM_COMMAND:
+		{
+			return CommandHandler(hWnd, uMsg, wParam, lParam);
+			break;
+		}
+		default:
+			return DefWindowProc(hWnd, uMsg, wParam, lParam);
+	}
+}
+
+
+LRESULT CommandHandler(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (wParam)
+	{
+
+		default:
+			return DefWindowProc(hWnd, uMsg, wParam, lParam);
+	}
+}
---
+++ main.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "base.h"
+
+LRESULT CALLBACK MainWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+WNDCLASS NewWindowClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON Icon, LPCWSTR Name, WNDPROC Procedure);
+LRESULT CommandHandler(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
