1da27ca502f7ea42ee67af2b49d31a23fb91abee
Никита Фомин <defdXs@yandex.ru>
2023-04-11 20:19:04 +0300

    чё-то накодил

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ using namespace std;
 //		GLOBAL VARIABLES
 //
 HWND MainWnd = { };
+HANDLE ServerThread = { };
+BOOL running = TRUE;
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow) {
 
@@ -22,8 +24,6 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdsho
 		DispatchMessage(&MainWndMessage);
 	}
 }
-
-
 WNDCLASS NewWindowClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON Icon, LPCWSTR Name, WNDPROC Procedure) {
 	WNDCLASS NWC = { 0 };
 
@@ -36,8 +36,6 @@ WNDCLASS NewWindowClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON I
 
 	return NWC;
 }
-
-
 LRESULT CALLBACK MainWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
@@ -61,8 +59,6 @@ LRESULT CALLBACK MainWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			return DefWindowProc(hWnd, uMsg, wParam, lParam);
 	}
 }
-
-
 LRESULT CommandHandler(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (wParam)
@@ -72,8 +68,51 @@ LRESULT CommandHandler(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			return DefWindowProc(hWnd, uMsg, wParam, lParam);
 	}
 }
-
 void OnMainWindowCreated(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	
+}
+void MB(string _Msg, string _End, UINT _Style)
+{
+	MessageBoxA(NULL, (_Msg + _End).c_str(), "Да", _Style);
+}
+DWORD WINAPI ServerHandler(LPVOID lpParam)
+{
+	SOCKADDR_IN addr;
+	int size = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr(DEFAULT_IP);
+	addr.sin_port = htons(DEFAULT_PORT);
+	addr.sin_family = AF_INET;
+
+	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);
+	bind(sListen, (SOCKADDR*)&addr, sizeof(addr));
+	listen(sListen, SOMAXCONN);
+
+	SOCKET newConnection;
+
+	MB("Waiting for new connections...");
+
+	return 1;
+}
+void Init()
+{
+	WSADATA WSAdata;
+	WORD DLLVersion = MAKEWORD(2, 1);
+	if (WSAStartup(DLLVersion, &WSAdata) != 0)
+	{
+		MB("no wsa cry :(");
+		PostQuitMessage(0);
+	}
+	ServerThread = CreateThread(
+		NULL,
+		0,
+		ServerHandler,
+		SERVER_T,
+		0,
+		NULL);
+}
+void Stop()
+{
+	running = FALSE;
+	WSACleanup();
 }
--- main.h
+++ main.h
@@ -3,11 +3,15 @@
 #include "base.h"
 
 #define MAIN_WC L"MainWndClass"
+#define SERVER_T 0
+#define DEFAULT_PORT 1111
+#define DEFAULT_IP "127.0.01"
 
 LRESULT CALLBACK MainWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 WNDCLASS NewWindowClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON Icon, LPCWSTR Name, WNDPROC Procedure);
 LRESULT CommandHandler(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 void OnMainWindowCreated(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-
-void ServerLoop(void*);
-void ClientLoop();
+void MB(string _Msg, string _End = "\r\n", UINT _Style = MB_OK | MB_ICONERROR);
+DWORD WINAPI ServerHandler(LPVOID lpParam);
+void Init();
+void Stop();
