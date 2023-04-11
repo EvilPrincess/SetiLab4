19b58d3b08a3889556386f76e9c84b5d337cbb03
Никита Фомин <defdXs@yandex.ru>
2023-04-11 21:57:57 +0300

    Скопировал всё из сервера чтобы удобно да.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "main.h"
 
-using namespace std;
-
 //
 //		GLOBAL VARIABLES
 //
 HWND MainWnd = { };
+HWND EditBox = { };
+HWND StartBtn = { };
+HWND StopBtn = { };
+HANDLE ServerThread = { };
+BOOL running = FALSE;
+CHAR BUFFER[BUFFERSIZE] = { };
+
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow) {
 
@@ -16,14 +21,12 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdsho
 
 	MSG MainWndMessage = { };
 
-	MainWnd = CreateWindow(MAIN_WC, L"Так называемый почтовый клиент", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 1200, 700, NULL, NULL, NULL, NULL);
+	MainWnd = CreateWindow(MAIN_WC, L"Так называемый почтовый сервер", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 1200, 700, NULL, NULL, NULL, NULL);
 	while (GetMessage(&MainWndMessage, NULL, NULL, NULL)) {
 		TranslateMessage(&MainWndMessage);
 		DispatchMessage(&MainWndMessage);
 	}
 }
-
-
 WNDCLASS NewWindowClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON Icon, LPCWSTR Name, WNDPROC Procedure) {
 	WNDCLASS NWC = { 0 };
 
@@ -36,44 +39,177 @@ WNDCLASS NewWindowClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON I
 
 	return NWC;
 }
-
-
 LRESULT CALLBACK MainWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
-		case WM_CREATE:
-		{
-			OnMainWindowCreated(hWnd, uMsg, wParam, lParam);
-			break;
-		}
-		case WM_CLOSE:
-		{
-			PostQuitMessage(0);
-			break;
-		}
-		case WM_COMMAND:
-		{
-			return CommandHandler(hWnd, uMsg, wParam, lParam);
-			break;
-		}
-		default:
-			return DefWindowProc(hWnd, uMsg, wParam, lParam);
+	case WM_CREATE:
+	{
+		OnMainWindowCreated(hWnd, uMsg, wParam, lParam);
+		break;
+	}
+	case WM_CLOSE:
+	{
+		PostQuitMessage(0);
+		break;
+	}
+	case WM_COMMAND:
+	{
+		return CommandHandler(hWnd, uMsg, wParam, lParam);
+		break;
+	}
+	case WM_SIZE:
+	{
+		OnResize(hWnd);
+		return 0;
+	}
+	case WM_MOVE:
+	{
+		OnResize(hWnd);
+		break;
+	}
+	default:
+		return DefWindowProc(hWnd, uMsg, wParam, lParam);
 	}
 }
+inline void OnResize(HWND hWnd)
+{
+	ResizeWindows(hWnd);
+	DrawServer(hWnd);
+}
+inline void ResizeWindows(HWND hWnd)
+{
+	RECT r;
+	GetClientRect(hWnd, &r);
+	MoveWindow(EditBox, 11, 11, r.right - 32, r.bottom - 112, TRUE);
+	MoveWindow(StartBtn, 10, r.bottom - 90, r.right / 2 - 20, 80, TRUE);
+	MoveWindow(StopBtn, r.right / 2, r.bottom - 90, r.right / 2 - 20, 80, TRUE);
+}
+inline void DrawServer(HWND hWnd)
+{
+	HDC hDC;
+	RECT r;
 
+	InvalidateRect(hWnd, NULL, TRUE);
+	UpdateWindow(hWnd);
 
+	hDC = GetDC(hWnd);
+	GetClientRect(hWnd, &r);
+
+	Rectangle(hDC, 10, 10, r.right - 20, r.bottom - 100);
+
+	ReleaseDC(hWnd, hDC);
+}
 LRESULT CommandHandler(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (wParam)
 	{
-
-		default:
-			return DefWindowProc(hWnd, uMsg, wParam, lParam);
+	case OnStartPressed:
+	{
+		Init();
+		break;
+	}
+	case OnStopPressed:
+	{
+		Stop();
+		break;
+	}
+	default:
+		return DefWindowProc(hWnd, uMsg, wParam, lParam);
 	}
 }
-
 void OnMainWindowCreated(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	
+	DrawServer(hWnd);
+	CreateWidgets(hWnd);
+
+}
+void CreateWidgets(HWND hWnd)
+{
+	RECT r;
+	GetClientRect(hWnd, &r);
+	EditBox = CreateWindowA("EDIT", "", WS_CHILD | WS_VISIBLE | ES_MULTILINE | ES_READONLY |
+		ES_AUTOVSCROLL | WS_VSCROLL, 11, 11, r.right - 22, r.bottom - 112, hWnd, NULL, NULL, NULL);
+	StartBtn = CreateWindowA("BUTTON", "Включить", WS_CHILD | WS_VISIBLE | SS_CENTER,
+		10, r.bottom - 90, r.right / 2 - 20, 80, hWnd, (HMENU)OnStartPressed, NULL, NULL);
+	StopBtn = CreateWindowA("BUTTON", "Отключить", WS_CHILD | WS_VISIBLE | SS_CENTER,
+		r.right / 2, r.bottom - 90, r.right / 2 - 20, 80, hWnd, (HMENU)OnStopPressed, NULL, NULL);
+}
+void MB(string _Msg, BOOL _IsWarning)
+{
+	MessageBoxA(NULL, _Msg.c_str(), "Да", _IsWarning ? MB_OK | MB_ICONERROR : MB_OK);
+}
+void DM(string _Msg, string _End)
+{
+	GetWindowTextA(EditBox, BUFFER, BUFFERSIZE);
+	SetWindowTextA(EditBox, (string(BUFFER) + _Msg + _End).c_str());
+}
+DWORD WINAPI ServerHandler(LPVOID lpParam)
+{
+	SOCKADDR_IN addr;
+	int size = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr(DEFAULT_IP);
+	addr.sin_port = htons(DEFAULT_PORT);
+	addr.sin_family = AF_INET;
+
+	SOCKET server;
+	if ((server = socket(AF_INET, SOCK_STREAM, NULL)) == SOCKET_ERROR)
+	{
+		MB("Ошибка функции socket: " + to_string(WSAGetLastError()), TRUE);
+		return 1;
+	}
+	if ((bind(server, (SOCKADDR*)&addr, sizeof(addr))) == SOCKET_ERROR)
+	{
+		MB("Ошибка функции bind: " + to_string(WSAGetLastError()), TRUE);
+		return 1;
+	}
+	DM("Успешное создание сервера!");
+
+	if ((listen(server, SOMAXCONN)) == SOCKET_ERROR)
+	{
+		MB("Ошибка функции listen: " + to_string(WSAGetLastError()), TRUE);
+		return 1;
+	}
+
+	DM("Ожидание клиентов...");
+	SOCKET client;
+	if ((client = accept(server, (SOCKADDR*)&addr, &size)) == SOCKET_ERROR)
+	{
+		MB("Ошибка функции accept: " + to_string(WSAGetLastError()), TRUE);
+	}
+
+
+	return 1;
+}
+void Init()
+{
+	if (running) return;
+	WSADATA WSAdata;
+	WORD DLLVersion = MAKEWORD(2, 1);
+	if (WSAStartup(DLLVersion, &WSAdata) != 0)
+	{
+		MB("no wsa cry :(");
+		PostQuitMessage(0);
+	}
+	running = TRUE;
+	ServerThread = CreateThread(
+		NULL,
+		0,
+		ServerHandler,
+		SERVER_T,
+		0,
+		NULL);
+}
+void Stop()
+{
+	if (!running) return;
+	running = FALSE;
+	if (WSACleanup() == SOCKET_ERROR)
+	{
+		MB("Ошибка функции WSACleanup", MB_OK | MB_ICONERROR);
+	}
+	else
+	{
+		DM("Сервер отключен.");
+	}
 }
--- main.h
+++ main.h
@@ -3,11 +3,26 @@
 #include "base.h"
 
 #define MAIN_WC L"MainWndClass"
+#define SERVER_T 0
+#define BUFFERSIZE 16384
+#define DEFAULT_PORT 1111
+#define DEFAULT_IP "127.0.01"
+
+//		COMMANDS
+//
+#define OnStartPressed 0
+#define OnStopPressed 1
 
 LRESULT CALLBACK MainWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 WNDCLASS NewWindowClass(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON Icon, LPCWSTR Name, WNDPROC Procedure);
 LRESULT CommandHandler(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 void OnMainWindowCreated(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-
-void ServerLoop(void*);
-void ClientLoop();
+void MB(string _Msg, BOOL _IsWarning = FALSE);
+void DM(string _Msg, string _End = "\r\n");
+DWORD WINAPI ServerHandler(LPVOID lpParam);
+void Init();
+void Stop();
+inline void DrawServer(HWND hWnd);
+inline void OnResize(HWND hWnd);
+inline void ResizeWindows(HWND hWnd);
+void CreateWidgets(HWND hWnd);
